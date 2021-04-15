cdb5b117eeefcfaade2680bdca51a46398c12744
shboulz <shboul89@gmail.com>
2021-04-15 14:13:23 -0400

    Data Commit

--- AgileProject.Data/Body.cs
+++ AgileProject.Data/Body.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AgileProject.Data
 {
-    class Body
+    public class Body
     {
+        [Key]
+        public int BodyId { get; set; }
+
+        [Required]
+        public string BodyType { get; set; }
+
+        [Required]
+        public string BodyDescription { get; set; }
+
+        [Required]
+        public Guid CharId { get; set; }
+
     }
 }
